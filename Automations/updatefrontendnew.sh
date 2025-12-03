375acba2076a2839bdc3548080c038b7b9accf1c
abhayth242907 <abhaythakur0055@gmail.com>
2025-12-04 04:25:59 +0530

    Update updatefrontendnew.sh

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-INSTANCE_ID="i-0a4e3a2feba169a50"
+INSTANCE_ID="i-0b7970a8bbaeceda6"  
+AWS_REGION="eu-west-1"
 
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
-  --query 'Reservations[0].Instances[0].PublicIpAddress' \
-  --output text)
+PUBLIC_IP=$(aws ec2 describe-instances \
+    --instance-ids "$INSTANCE_ID" \
+    --region "$AWS_REGION" \
+    --query "Reservations[0].Instances[0].PublicIpAddress" \
+    --output text)
 
-file_to_find="../frontend/.env.docker"
+FRONTEND_URL="http://$PUBLIC_IP:3000"
 
-if [ ! -f "$file_to_find" ]; then
-  echo "ERROR: $file_to_find not found."
-  exit 1
-fi
+sed -i "s|REACT_APP_API_BASE_URL=.*|REACT_APP_API_BASE_URL=$FRONTEND_URL|g" ../frontend/.env.docker
 
-sed -i -e "s|REACT_APP_API_BASE_URL=.*|REACT_APP_API_BASE_URL=http://${ipv4_address}:8000|g" $file_to_find
-
-echo "Updated frontend REACT_APP_API_BASE_URL with: http://${ipv4_address}:8000"
+echo "Updated frontend REACT_APP_API_BASE_URL with: $FRONTEND_URL"
